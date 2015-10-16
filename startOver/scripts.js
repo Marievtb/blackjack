b7bdb35290fcbc6fb55d0e2d35fa3a48ce35c2fc
Marie Bluett <marie.bluett@gmail.com>
2015-10-16 00:29:38 -0400

    function pseudo

--- startOver/scripts.js
+++ startOver/scripts.js
@@ -1,15 +1,58 @@
-// console.log("ksahfkadjhsdf");
+// // console.log("ksahfkadjhsdf");
 
-function cards(){
-	var face = ['A','2', '3', '4', '5', '6', '7', '8', '9', '10','J', 'Q', 'K'];
-	var suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-	var value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10,10, 10];
-	for(var f = 0; f < face.length; f++){
-		for(var s = 0; s < suits.length; s++){
-			for(var v = 0; v < value.length; v++){
-			} 
+// An array of cards
+// values
+// faces
+// suits
+
+// function randomCards(){
+// 	math.random to pull indexes (*2)
+// 	dealbutton.onclick 
+// }
+
+
+// function player1Cards{
+// 	innerHTML to player 1 card Div
+// }
+
+// function player2Cards{
+// 	innerHTML in player2cardDiv
+// }
+
+// if player1cards > 21{
+// 	console.log(player1: + "bust!");
+// }
+// else if player2cards> 21{
+// 	console.log(player2 = "Bust!");
+// }
+
+// for 
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ============
+// Trial and Error
+// ============
+// function cards(){
+// 	var face = ['A','2', '3', '4', '5', '6', '7', '8', '9', '10','J', 'Q', 'K'];
+// 	var suit = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+// 	var value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10,10, 10];
+// 	for(var f = 0; f < face.length; f++){
+// 		for(var s = 0; s < suits.length; s++){
+// 			for(var v = 0; v < value.length; v++){
+// 			} 
 		
-		}
-	} 
-}
-cards()
+// 		}
+// 	} 
+// }
+// cards()
